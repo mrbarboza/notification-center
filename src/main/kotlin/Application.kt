4f74a55fc336d448b7ee1daca0088939b1fafed4
Miguel Barboza <mrbarboza@icloud.com>
2023-09-17 09:58:06 -0300

    :tada: Initial Commit

---
+++ src/main/kotlin/Application.kt
@@ -0,0 +1,17 @@
+package com.mrbarboza
+
+import com.mrbarboza.plugins.*
+import io.ktor.server.application.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+
+fun main() {
+    embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module)
+        .start(wait = true)
+}
+
+@Suppress("unused")
+fun Application.module() {
+    configureRouting()
+    contentNegotiation()
+}
---
+++ src/main/kotlin/plugins/Routing.kt
@@ -0,0 +1,8 @@
+package com.mrbarboza.plugins
+
+import io.ktor.server.application.*
+import io.ktor.server.routing.*
+
+fun Application.configureRouting() {
+    routing {}
+}
