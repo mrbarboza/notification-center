39f70601a9fab36671d5c577bd73eb50d036ef35
Miguel Barboza <mrbarboza@icloud.com>
2023-09-18 13:19:18 -0300

    :sparkles: feat: basic health-check

--- src/main/kotlin/plugins/Routing.kt
+++ src/main/kotlin/plugins/Routing.kt
@@ -1,8 +1,18 @@
 package com.mrbarboza.plugins
 
+import io.ktor.http.*
 import io.ktor.server.application.*
+import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
 fun Application.configureRouting() {
-    routing {}
+    routing {
+        get("/health") {
+            call.respond(HttpStatusCode.OK, "Ok")
+        }
+
+        route("/api") {
+            route("v1") {}
+        }
+    }
 }
